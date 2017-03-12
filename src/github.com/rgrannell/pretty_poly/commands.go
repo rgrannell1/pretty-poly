2b3067bdac16ae0db09899377077a018f4c1c702
rgrannell1 <r.grannell2@gmail.com>
2017-03-12 22:02:12 +0000

    added basic type-wrapper

---
+++ src/github.com/rgrannell/pretty_poly/commands.go
@@ -0,0 +1,27 @@
+
+package pretty_poly
+
+
+
+
+type SolveArgs struct {
+	Extreme    int
+	Order      int
+	FileName   string
+}
+
+func Solve (arguments SolveArgs) {
+
+}
+
+
+
+
+
+type DrawArgs struct {
+
+}
+
+func Draw (arguments DrawArgs) {
+
+}
--- src/github.com/rgrannell/pretty_poly_cli/main.go
+++ src/github.com/rgrannell/pretty_poly_cli/main.go
@@ -4,10 +4,32 @@ package main
 
 
 
-import "fmt"
 import "github.com/docopt/docopt-go"
-//import "github.com/rgrannell/pretty_poly"
+import "github.com/rgrannell/pretty_poly"
 
+
+
+
+
+const commandUsage = `
+Usage:
+	pretty_poly solve --name <str> [--extreme <num>] [--order <num>]
+
+	pretty_poly -h | --help
+	pretty_poly --version
+
+Description:
+	.
+
+Options:
+	--name <str>       .
+
+	--extreme <num>    The largest integer coefficient to use [default: 5].
+	--order <num>      The order of the polynomial to solve [default: 3].
+
+	-h, --help         Show this documentation.
+	--version          Show the package version.
+`
 
 
 
@@ -15,24 +37,26 @@ import "github.com/docopt/docopt-go"
 
 func main ( ) {
 
-	usage := `Pretty Poly
-
-	Usage:
-		pretty_poly -h | --help
-		pretty_poly --version
-
-	Options:
-		-h, --help    Show this documentation.
-		--version     Show the package version.
-
-	`
-
-	arguments, err := docopt.Parse(usage, nil, true, "Pretty Poly 0.1", false)
+	args, err := docopt.Parse(commandUsage, nil, true, "Pretty Poly 0.1", false)
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println( arguments )
+	if args["solve"] == true {
+
+		pretty_poly.Solve(pretty_poly.SolveArgs {
+			Order:    args["--order"].(int),
+			Extreme:  args["--extreme"].(int),
+			FileName: args["--name"].(string),
+		})
+
+	} else if args["draw"] == true {
+
+		pretty_poly.Draw(pretty_poly.DrawArgs {
+
+		})
+
+	}
 
 }
