604a3e13e64ea8adea033433d7377da8d49da535
rgrannell1 <r.grannell2@gmail.com>
2017-07-23 19:13:01 +0100

    initial commit of refactored branch-code

--- src/github.com/rgrannell/pretty_poly/events.go
+++ src/github.com/rgrannell/pretty_poly/events.go
@@ -6,5 +6,7 @@ package pretty_poly
 
 
 
-const EVENT_DRAW_IMAGE = "draw-image"
-const EVENT_DRAW_READ  = "draw-read"
+const EVENT_DRAW_IMAGE      = "draw-image"
+const EVENT_DRAW_READ       = "draw-read"
+const EVENT_DRAW_READ_DONE  = "draw-read-done"
+const EVENT_DRAWN           = "draw"
--- src/github.com/rgrannell/pretty_poly/pretty_poly.go
+++ src/github.com/rgrannell/pretty_poly/pretty_poly.go
@@ -2,11 +2,9 @@
 package pretty_poly
 
 import "log"
-import "sync"
 import "math"
 import "io"
 import "os"
-import "bufio"
 import "encoding/binary"
 import "image"
 import "image/png"
@@ -15,63 +13,48 @@ import "github.com/alash3al/goemitter"
 
 
 
+func solveRange (offset int, exploredBases [ ] int, extreme int, processes int) chan [ ]complex128 {
 
+	coefficientCount := productOf(exploredBases)
+	solutions        := make(chan [ ]complex128, processes)
 
-type appArguments struct {
+	go func ( ) {
 
-	precision  int8
-	dimensions interval2d
-
-}
-
-
-
-
-func processSolution (solution complex128, args appArguments) (uint64, error) {
-
-	argandPoint := point2d {
-		x: real(solution),
-		y: imag(solution),
-	}
-
-	return Geohash2dAsUint64(
-		Geohash2d(args.precision, args.dimensions, argandPoint),
-	)
-
-}
-
-
-
-
-
-func writeManager (filepath string, solutionsChan chan [ ] complex128, writeChan chan uint64) {
-
-	conn, err := os.Create(filepath)
-	defer conn.Close( )
-
-	writer := bufio.NewWriter(conn)
-	defer writer.Flush( )
-
-	if err != nil {
-		panic(err)
-	}
-
-	count := 0
-
-	for solution := range writeChan {
-
-		buffer := make([ ] byte, 8, 8)
-		binary.LittleEndian.PutUint64(buffer, solution)
-
-		for _, uintByte := range buffer {
-			writer.WriteByte(uintByte)
+		for ith := offset; ith < coefficientCount; ith += processes {
+			solutions <- solvePolynomial( toCompanionMatrix(toMixedRadix(exploredBases, ith), float64(extreme)) )
 		}
 
-		count++
+	}( )
+
+	return solutions
+
+}
+
+
+
+
+
+func mergeSolutions (inputs [ ] chan [ ]complex128) chan [ ]complex128 {
+
+	output := make(chan [ ]complex128)
+
+	for _, input := range inputs {
+
+		go func (input chan [ ]complex128) {
+
+			for solution := range input {
+				output <- solution
+			}
+
+		}(input)
 
 	}
 
+	return output
+
 }
+
+
 
 
 
@@ -79,12 +62,10 @@ func SolvePolynomials (extreme int, order int, filepath string, precision int8) 
 
 	processes := 20
 
-	args := appArguments {
-		precision:  precision,
-		dimensions: Interval2d(-10, 10, -10, 10),
-	}
-
-	var solveGroup sync.WaitGroup
+//	args := appArguments {
+//		precision:  precision,
+//		dimensions: Interval2d(-10, 10, -10, 10),
+//	}
 
 	bases := [ ] int { }
 
@@ -92,53 +73,23 @@ func SolvePolynomials (extreme int, order int, filepath string, precision int8) 
 		bases = append(bases, extreme)
 	}
 
-	solutionsChan := make(chan [ ] complex128, 100)
 	exploredBases := make([ ] int, order)
 
 	for ith := 0; ith < order; ith++ {
 		exploredBases[ith] = (2 * extreme) + 1
 	}
 
-	for offset := 0; offset < processes; offset++ {
+	var solutions [ ] chan [ ]complex128
 
-		solveGroup.Add(1)
-
-		go func (offset int) {
-
-			defer solveGroup.Done( )
-
-			coefficientCount := productOf(exploredBases)
-
-			for ith := offset; ith < coefficientCount; ith += processes {
-				solutionsChan <- solvePolynomial( toCompanionMatrix(toMixedRadix(exploredBases, ith), float64(extreme)) )
-			}
-
-		}(offset)
-
+	for offset := 0; offset < processes; offset ++ {
+		solutions = append(solutions, solveRange(offset, exploredBases, extreme, processes))
 	}
 
-	writeChan := make(chan uint64, 100)
+	for x := range mergeSolutions(solutions) {
+		println(x)
+	}
 
-	go writeManager(filepath, solutionsChan, writeChan)
-	go func ( ) {
-
-		for solutions := range solutionsChan {
-			for _, solution := range solutions {
-
-				encoded, err := processSolution(solution, args)
-
-				if err != nil {
-					panic("aarrgghh!")
-				}
-
-				writeChan <- encoded
-
-			}
-		}
-
-	}( )
-
-	solveGroup.Wait( )
+//	writeSolutions(filepath, solutions, args)
 
 }
 
@@ -150,11 +101,10 @@ func DrawImage (solutionPath string, precision float64) error {
 	conn, err := os.OpenFile("/var/log/pretty-poly.log", os.O_WRONLY | os.O_CREATE | os.O_APPEND, 0644)
 
 	if err != nil {
-		return err
+		log.SetOutput(conn)
 	}
 
 	defer conn.Close( )
-	log.SetOutput(conn)
 
 	buffer := make([ ] byte, 8)
 	logger := Emitter.Construct( )
@@ -228,7 +178,7 @@ func DrawImage (solutionPath string, precision float64) error {
 	for ith := dimensions.x.lower; ith < dimensions.x.upper; ith++ {
 		for jth := dimensions.y.lower; jth < dimensions.y.upper; jth++ {
 
-			img.content.Set(int(ith), int(jth), color.RGBA {
+			img.	.Set(int(ith), int(jth), color.RGBA {
 				25,
 				25,
 				25,
